0b324c7f96bd4a6d657efbb9135a01a84d62fb8f
SeyoungKo <airseyoung@naver.com>
2021-04-02 16:02:14 +0900

    Add : manager info interface

---
+++ pocket_cafe_client/app/src/main/java/util/Action.java
@@ -0,0 +1,11 @@
+package util;
+
+public interface Action {
+
+    String MANAGER_MAIN_NICKNAME = "manager_main_nickname";
+    String MANAGER_CHANGE_INFO = "manager_change_info";
+    String MANAGER_STORE = "manager_store";
+    String MANAGER_MENU_STORE_IDX = "manager_menu_store_idx";
+    String MANAGER_MENU = "manager_menu";
+
+}
